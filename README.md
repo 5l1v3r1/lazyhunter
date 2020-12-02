037df5a3b92569f2abc2aa2425d61491b15ee631
Dr. Signed <>
2020-12-02 18:40:12 +0300

    -

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 ![Made with Bash](https://img.shields.io/badge/made%20with-Bash-0040ff.svg) ![Maintenance](https://img.shields.io/badge/maintained%3F-yes-0040ff.svg) [![open issues](https://img.shields.io/github/issues-raw/drsigned/adscript.svg?style=flat&color=0040ff)](https://github.com/drsigned/adscript/issues?q=is:issue+is:open) [![closed issues](https://img.shields.io/github/issues-closed-raw/drsigned/adscript.svg?style=flat&color=0040ff)](https://github.com/drsigned/adscript/issues?q=is:issue+is:closed) [![License](https://img.shields.io/badge/License-MIT-gray.svg?colorB=0040FF)](https://github.com/drsigned/adscript/blob/master/LICENSE) [![author](https://img.shields.io/badge/twitter-@drsigned-0040ff.svg)](https://twitter.com/drsigned)
 
 A wrapper, bash script, around tools I use for assets discovery to automate my workflow. This script utilizes the following tools:
-* Subdomain Enumeration
+* Subdomains Gathering
     * [amass]()
     * [sigsubs]()
     * [subfinder]()
     * [findomain]()
-* Subdomain Resolution
+* Subdomains Resolution
     * [massdns]()
 * HTTP(S) probing
     * [httpx]()
--- adscript.sh
+++ adscript.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+bold="\e[1m"
+red="\e[31m"
+cyan="\e[36m"
+blue="\e[34m"
+reset="\e[0m"
+green="\e[32m"
+yellow="\e[33m"
+underline="\e[4m"
 script_file_name=${0##*/}
 
 banner() {
-echo -e "
+echo -e ${bold}${blue}"
            _               _       _   
   __ _  __| |___  ___ _ __(_)_ __ | |_ 
  / _\` |/ _\` / __|/ __| '__| | '_ \| __|
 | (_| | (_| \__ \ (__| |  | | |_) | |_ 
- \__,_|\__,_|___/\___|_|  |_| .__/ \__| v1.0.0
-----------------------------|_| By Dr. Signed -----
----------------------------------------------------"
+ \__,_|\__,_|___/\___|_|  |_| .__/ \__| ${green}v1.0.0 ${blue}
+----------------------------|_| ${yellow}By Dr. Signed ${blue}-----
+---------------------------------------------------"${reset}
 }
 
 display_usage() {
@@ -25,16 +33,15 @@ display_usage() {
 
 	\r Options:
 	\r   -d, --domain\t\t domain to AD on
+	\r   -u, --use\t\t subs' sources to use
 	\r   -e, --exclude\t subs' sources to exclude
-	\r   -h, --help\t\t display this message and exit
-	\r       --httprobe\t http(s) probe
-	\r   -k, --keep\t\t keep each tool's temp results
-	\r   -n, --notify\t\t send notifications
-	\r   -o, --output\t\t output directory path
 	\r       --resolve\t resolve subdomains
 	\r       --screenshot\t take screenshots
-	\r   -u, --use\t\t subs' sources to use
-
+	\r   -o, --output\t\t output directory path
+	\r   -k, --keep\t\t keep each tool's temp results
+	\r   -n, --notify\t\t send notifications
+	\r   -h, --help\t\t display this message and exit
+	
 	\r HAPPY HACKING ! :)
 
 EOF
@@ -69,51 +76,52 @@ check_tools() {
 _amass() {
 	local amass_output="${output}/temp-amass-subdomains.txt"
 
-	printf "    [+] amass"
+	printf "    [${green}+${reset}] amass"
 	printf "\r"
 	amass enum -d ${domain} -o ${amass_output} &> /dev/null
-	echo -e "    [+] amass: $(wc -l < ${amass_output})"
+	echo -e "    [${green}+${reset}] amass: $(wc -l < ${amass_output})"
 }
 
 _sigsubs() {
 	local sigsubs_output="${output}/temp-sigsubs-subdomains.txt"
 
-	printf "    [+] sigsubs"
+	printf "    [${green}+${reset}] sigsubs"
 	printf "\r"
 	sigsubs -d ${domain} -s 1> ${sigsubs_output} 2> /dev/null
-	echo -e "    [+] sigsubs: $(wc -l < ${sigsubs_output})"
-}
-
-_subfinder() {
-	local subfinder_output="${output}/temp-subfinder-subdomains.txt"
-
-	printf "    [+] subfinder"
-	printf "\r"
-	subfinder -d ${domain} -silent 1> ${subfinder_output} 2> /dev/null
-	echo -e "    [+] subfinder: $(wc -l < ${subfinder_output})"
+	echo -e "    [${green}+${reset}] sigsubs: $(wc -l < ${sigsubs_output})"
 }
 
 _findomain() {
 	local findomain_output="${output}/temp-findomain-subdomains.txt"
 
-	printf "    [+] findomain"
+	printf "    [${green}+${reset}] findomain"
 	printf "\r"
 	findomain -t ${domain} -q 1> ${findomain_output} 2> /dev/null
-	echo -e "    [+] findomain: $(wc -l ${findomain_output} | awk '{print $1}' 2> /dev/null)"
+	echo -e "    [${green}+${reset}] findomain: $(wc -l ${findomain_output} | awk '{print $1}' 2> /dev/null)"
+}
+
+_subfinder() {
+	local subfinder_output="${output}/temp-subfinder-subdomains.txt"
+
+	printf "    [${green}+${reset}] subfinder"
+	printf "\r"
+	subfinder -d ${domain} -silent 1> ${subfinder_output} 2> /dev/null
+	echo -e "    [${green}+${reset}] subfinder: $(wc -l < ${subfinder_output})"
 }
 
 main() {
 	check_tools
-
 	banner
 
-	echo
-	echo -e "[*] asset discovery on ${domain}"
-	echo
+	echo -e "
+[${yellow}*${reset}] asset discovery on ${bold}${underline}${domain}${reset}
+"
 
 	[ ! -d ${output} ] && mkdir -p ${output}
 
-	echo -e "[+] subdomain enumeration"
+	# {{ SUNDOMAIN GATHERING
+
+	echo -e "[${green}+${reset}] subdomains gathering"
 
 	subdomains="${output}/subdomains.txt"
 
@@ -143,39 +151,44 @@ main() {
 	}
 
 	cat ${output}/temp-*-subdomains.txt | sed 's#*.# #g' | anew -q ${subdomains}
-
 	echo -e "    [=] unique subdomains: $(wc -l < ${subdomains})"
 
 	[ ${keep} == False ] && rm ${output}/temp-*-subdomains.txt
 
+	# }}
+	# {{ SUBDOMAINS RESOLUTION
+
 	[ ${resolve} == True ] && {
-		echo -e "[+] subdomain resolution"
+		echo -e "[${green}+${reset}] subdomain resolution"
 
 		ips="${output}/ips.txt"
 		resolved_subdomains="${output}/resolved-subdomains.txt"
 
 		local massdns_output="${output}/temp-massdns-resolve.txt"
 
-		printf "    [+] resolving"
+		printf "    [${green}+${reset}] resolving"
 		printf "\r"
 		massdns -r ${HOME}/tools/web-security/discovery/dns/massdns/lists/resolvers.txt -q -t A -o S -w ${massdns_output} ${subdomains}
-		echo -e "    [+] resolved!"
+		echo -e "    [${green}+${reset}] resolved!"
 
-		printf "    [+] resolved IPs"
+		printf "    [${green}+${reset}] resolved IPs"
 		printf "\r"
 		egrep -o -h "[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}" ${massdns_output} | sort -u | anew -q ${ips}
-		echo -e "    [+] resolved IPs: $(wc -l < ${ips})"
+		echo -e "    [${green}+${reset}] resolved IPs: $(wc -l < ${ips})"
 
-		printf "    [+] resolved subdomains"
+		printf "    [${green}+${reset}] resolved subdomains"
 		printf "\r"
 		cat ${massdns_output} | grep -Po "^[^-*\"]*?\K[[:alnum:]-]+\.${domain}" | sort -u | anew -q ${resolved_subdomains}
-		echo -e "    [+] resolved subdomains: $(wc -l < ${resolved_subdomains})"
+		echo -e "    [${green}+${reset}] resolved subdomains: $(wc -l < ${resolved_subdomains})"
 
 		[ ${keep} == False ] && rm ${output}/temp-*-resolve.txt
 	}
 
+	# }}
+	# {{ HTTP(S) PROBING
+
 	[ ${resolve} == True ] && [ ${httprobe} == True ] && {
-		echo -e "[+] http(s) probing"
+		echo -e "[${green}+${reset}] http(s) probing"
 
 		hosts="${output}/hosts.txt"
 
@@ -186,8 +199,11 @@ main() {
 		}
 	}
 
+	# }}
+	# {{ VISUAL RECONNAISSANCE
+
 	[ ${resolve} == True ] && [ ${httprobe} == True ] &&[ ${screenshot} == True ] && {
-		echo -e "[+] visual reconnaissance"
+		echo -e "[${green}+${reset}] visual reconnaissance"
 
 		visual_reconnaissance="${output}/visual-reconnaissance"
 		[ ! -d ${visual_reconnaissance} ] && mkdir -p ${visual_reconnaissance}
@@ -195,7 +211,9 @@ main() {
 		cat ${hosts} | aquatone -http-timeout 10000 -out ${visual_reconnaissance} &> /dev/null
 	}
 
-	echo -e "\n- DONE! -------------------------------------------"
+	# }}
+
+	echo -e ${bold}${blue}"\n- DONE! -------------------------------------------"${reset}
 }
 
 # tasks
@@ -254,15 +272,15 @@ do
 			done
 			shift
 		;;
+		--resolve) resolve=True ;;
+		--httprobe) httprobe=True ;;
+		--screenshot) screenshot=True ;;
 		-o | --output)
 			output="${2}/asset-discovery"
 			shift
 		;;
-		--resolve) resolve=True ;;
-		--httprobe) httprobe=True ;;
-		--screenshot) screenshot=True ;;
-		-n | --notify) notify=True ;;
 		-k | --keep) keep=True ;;
+		-n | --notify) notify=True ;;
 		-h | --help) 
 			display_usage
 			exit 0
